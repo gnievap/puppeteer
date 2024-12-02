991b917dd3c53a85ad50524504138ed3f234c08b
Gaby Nieva <gaby.nieva@outlook.com>
2024-12-01 23:21:16 -0600

    Navegando con puppeteer

--- __tests__/navegador.test.js
+++ __tests__/navegador.test.js
@@ -2,11 +2,30 @@ const puppeteer = require('puppeteer')
 describe('Mi primer test en puppeteer', () =>{
     it ('Debe de abrir y cerrar el navegador', async () => {
         const browser = await puppeteer.launch({
-            headless:false
+            headless: true,
+            defaultViewport: null
         })
         
         const page = await browser.newPage()
         await page.goto('https://dcodingames.com')
+        await page.waitForSelector('img')
+        // Recargar la página
+        await page.reload()
+        await page.waitForSelector('img')
+
+        // Navegar a otro sitio
+        await page.goto('https://platzi.com')
+        //await page.waitForSelector('#home-public > div > div.BaseLayout > header > nav > div.Logo > div > a > div > figure:nth-child(1) > img')
+        
+        // Navegar hacia atrás
+        await page.goBack()
+        await page.goForward()
+        //await page.waitForSelector('img')
+
+        // abrir otra pagina en otra pestaña
+        const page2 = await browser.newPage()
+            await page2.goto('https://github.com')
+
         await browser.close()
-    },5000)
+    },55000)
 })
