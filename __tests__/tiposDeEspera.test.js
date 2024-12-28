165efc251c849c3c888cc2f3f136e18b044c8e2a
Gaby Nieva <gaby.nieva@outlook.com>
2024-12-27 22:10:25 -0600

    Esperando por funciones

--- __tests__/tiposDeEspera.test.js
+++ __tests__/tiposDeEspera.test.js
@@ -4,7 +4,8 @@ describe('Tipos de espera', () =>{
         const browser = await puppeteer.launch({
             headless: false,
             defaultViewport: null,
-            executablePath: 'C://Program Files//Google//Chrome//Application//chrome.exe'
+            executablePath: 'C://Program Files//Google//Chrome//Application//chrome.exe',
+            slowMo: 500
         })
         
         const page = await browser.newPage()
@@ -18,12 +19,25 @@ describe('Tipos de espera', () =>{
         //await page.waitForSelector('#home-public > div > div.BaseLayout > header > nav > div.Logo > div > a > div > figure:nth-child(1) > img')
 
         //Espera por un xpath
-        //await page.waitForXpath('//*[@id="home-public"]/div/div[2]/div[1]/div/a/d)
+        //await page.waitForXpath(' *[@id="home-public"]/div/div[2]/div[1]/div/a/d)
 
-        await page.goto('http://demoga.com/modal-dialogs', { waitUntil: 'networkidle0'})
-        //await page.waitForSelector('#showSmallModal', {visible: true})
+        await page.goto('https://demoqa.com/modal-dialogs')
+        await page.waitForSelector('#showSmallModal', {visible: true})
         const button = await page.waitForXPath('//*[@id="showSmallModal"]', {visible: true})
         await button.click()
+
+        // Espera por funcion 
+        await page.waitForFunction(()=> document.querySelector('#example-modal-sizes-title-sm').innerText === 'Small Modal')
+        
+        // Ejemplo para observar el viewport
+        // const observarResize = page.waitForFunction('window.innerWidth < 100')
+        // await page.setViewport({width: 50, height: 50})
+        // await observarResize
+
+        await page.click('#closeSmallModal')
+        await page.waitForFunction(()=> !document.querySelector('#example-modal-sizes-title-sm'))
+
+
         await browser.close()
     },55000)
 })
